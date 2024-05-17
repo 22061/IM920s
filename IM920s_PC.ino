9b83ff9468bd7cd3d98fb9b4ff07456b537de887
22061 <150252996+22061@users.noreply.github.com>
2024-05-17 16:44:44 +0900

    Update

--- IM920s_PC.ino
+++ IM920s_PC.ino
@@ -1,34 +1,35 @@
-#include<ArduinoJson.h>
-#include <SoftwareSerial.h>
+#include<SoftwareSerial.h>
+#include <MsTimer2.h>
 
-StaticJsonDocument<200> doc;
-SoftwareSerial IM920Serial(8, 9);    // RX, TX
+SoftwareSerial IM920Serial(8, 9);
 
-char data[50];
+char buf[256];
+void read_timer()
+{
+  int i = 0;
+  for(int j = 0; j < 256; j++)buf[j] = 0;
+
+  while(Serial.available() > 0)
+  {
+    buf[i] = Serial.read();
+    i++;
+
+    if(i >= 256)
+    {
+      break;
+    }
+  }
+}
 
 void setup() {
-  pinMode(A0, INPUT);        
-  pinMode(A1, INPUT);        
-  pinMode(5, INPUT_PULLUP);
-
+  // put your setup code here, to run once:
   Serial.begin(19200);
-  IM920Serial.begin(19200);  
+  IM920Serial.begin(19200);
+  MsTimer2::set(1000, read_timer);
+  MsTimer2::start();
 }
 
 void loop() {
-  int x_pos = map(analogRead(A0), 0, 1023, -1, 1);
-  int y_pos = map(analogRead(A1), 0, 1023, -1, 1);
-  int sw_pos = digitalRead(5);
-  
-  doc["x_axis"] = x_pos;
-  doc["y_axis"] = y_pos;
-  doc["z"] = sw_pos;
-  serializeJson(doc, data, 50);
-
-  //Serial.println(data);
-
-  IM920Serial.print("TXDU0002,");
-  IM920Serial.println(data);
-
-  delay(1000);
+  // put your main code here, to run repeatedly:
+  IM920Serial.println(buf);
 }
